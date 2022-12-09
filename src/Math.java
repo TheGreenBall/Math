c38a00d390e5d60b8da9855e2dcb238e17bafb3f
Mbanholzer <mbanholzer5017@stu.d214.org>
2022-12-09 14:37:39 -0600

    gg

--- src/Math.java
+++ src/Math.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Math {
+public class Math {
+
+    double E = 2.71828;
+    public static double PI = 3.14159;
+
+
+
+
+
+    static double abs(int x) {
+
+        if (x < 0){
+
+            x = x*1;
+            return x;
+
+
+
+        } else {
+            return x;
+        }
+
+
+
+    }
+
+    static double max(double x, double y) {
+
+        if (x > y) {
+            return x;
+        } else if (x < y) {
+            return y;
+        } else {
+            return x;
+        }
+
+    }
+
+    static double min(double x, double y) {
+
+        if (x > y) {
+            return y;
+        } else if (x < y) {
+            return x;
+        } else {
+            return y;
+        }
+
+
+    }
+
 }
